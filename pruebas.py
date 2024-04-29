030917724bb9b76677059c4838f36c624faa2c1a
100474964 <100474964@alumnos.uc3m.es>
2024-04-29 14:21:22 +0200

    Fuzzy age function

--- pruebas.py
+++ pruebas.py
@@ -24,6 +24,33 @@ def main():
                 age_mf = skf.interp_membership(fset.x, fset.y, age)
                 print("Membresia: " + str(age_mf))
 
+            if fset.var == lables[1]:
+                income = app.data[1][1]
+                income_mf = skf.interp_membership(fset.x, fset.y, income)
+                print("Membresia: " + str(income_mf))
+
+            if fset.var == lables[2]:
+                assets = app.data[2][1]
+                assets_mf = skf.interp_membership(fset.x, fset.y, assets)
+                print("Membresia: " + str(assets_mf))
+
+            if fset.var == lables[3]:
+                amount = app.data[3][1]
+                amount_mf = skf.interp_membership(fset.x, fset.y, amount)
+                print("Membresia: " + str(amount_mf))
+
+            if fset.var == lables[4]:
+                job = app.data[4][1]
+                job_mf = skf.interp_membership(fset.x, fset.y, job)
+                print("Membresia: " + str(job_mf))
+
+            if fset.var == lables[5]:
+                history = app.data[5][1]
+                history_mf = skf.interp_membership(fset.x, fset.y, history)
+                print("Membresia: " + str(history_mf))
+
+
+
         print("\n")
 
 
